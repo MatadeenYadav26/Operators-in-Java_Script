7e83da553e005d285c037d78aa0620a652bcffa9
KING <matadeenyadav202@gmail.com>
2026-01-17 11:39:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+// let a = 12;
+// a += 13;
+// a -= 23;
+// a *= 4;
+// a /= 2;
+// a %= 3;
+
+// if (!(12>13)){
+//     console.log("Hey!");
+// }
+
+// 12>13 ? console.log("hey") : console.log("hello") 
+
+// let arr = [];
+
+// let a = [];
+
+// a = "Aman"+"Yadav"
+
+// 12 > 13 ?? console.log("Sorry")
+// null ?? console.log("Sorry")
+
+// let obj = {
+//     name : {
+//         first : "Aman",
+//         second:{
+//             sf: "Yadav",
+//             sl : "Something",
+//         },
+//     },
+// };
+
+// obj.name.first
+
+// let objs = {}; will give error
+// objs.name.first
+// let objs = {}; 
+// objs?.name?.first  //using ? will save us from errors.
+
+// Variable hoisting:
+
+console.log(b)
+var b = 12;
